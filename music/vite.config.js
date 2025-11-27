88f9159a17a8ca4021821c4f7fe1f79dbf2a633c
Amelia1190 <lurbetamelialibertad@gmail.com>
2025-11-27 07:04:50 -0300

    Configura la base path para GH Pages

--- music/vite.config.js
+++ music/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ¡AÑADE O CORRIGE ESTA LÍNEA!
+  base: '/Pr-ctico-Segundo-Cuatrimestre-claro-oscuro/', 
   plugins: [react()],
-})
+});
